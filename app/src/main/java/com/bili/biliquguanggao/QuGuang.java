e87d5dc54b1724cb2bb404df243256a9c221716e
feilongzaitia <A2322146826@163.com>
2023-11-10 23:08:59 +0800

    修复bug

--- app/src/main/java/com/bili/biliquguanggao/QuGuang.java
+++ app/src/main/java/com/bili/biliquguanggao/QuGuang.java
@@ -17,12 +17,6 @@ public class QuGuang implements IXposedHookLoadPackage {
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         if (loadPackageParam.packageName.equals("tv.danmaku.bili")){
             Class SplashClazz = XposedHelpers.findClassIfExists("tv.danmaku.bili.ui.splash.ad.model.Splash",loadPackageParam.classLoader);
-
-            Class AdClazz = XposedHelpers.findClassIfExists("tv.danmaku.bili.ui.splash.ad.page.x",loadPackageParam.classLoader);
-            if (AdClazz == null){
-                Log.d(TAG,"版本过低没有适配");
-            }
-            Class SourceContentClazz = XposedHelpers.findClassIfExists("com.bilibili.adcommon.basic.model.SourceContent",loadPackageParam.classLoader);
             XposedHelpers.findAndHookMethod("tv.danmaku.bili.ui.splash.ad.page.x", loadPackageParam.classLoader, "a", SplashClazz, new XC_MethodHook() {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
@@ -38,18 +32,6 @@ public class QuGuang implements IXposedHookLoadPackage {
                             if (param.args[0].equals("code") && StackUtils.isCallingFromupdate()){
                                 Log.d(TAG,"hook2成功");
                                 param.setResult(304);
-                            }
-                            super.afterHookedMethod(param);
-                        }
-                    });
-
-            XposedHelpers.findAndHookMethod("com.bilibili.ad.adview.videodetail.b",
-                    loadPackageParam.classLoader, "a", SourceContentClazz, new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            if ((int)param.getResult() == 114){
-                                Log.d(TAG,"hook3成功");
-                                param.setResult(105);
                             }
                             super.afterHookedMethod(param);
                         }
